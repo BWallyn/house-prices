a819278e3c602cfd589215350ff93a9038fa5d0f
BWallyn <ben96.wallyn@gmail.com>
2023-12-07 10:35:38 +0100

    pipeline data science

---
+++ src/house_prices/pipelines/data_science/pipeline.py
@@ -0,0 +1,73 @@
+"""
+This is a boilerplate pipeline 'data_science'
+generated using Kedro 0.18.14
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import feature_engineering, define_search_space, find_best_hyperparameters, recreate_training,\
+    train_model_mlflow, train_model, predict_model
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([
+        node(
+            func=feature_engineering,
+            inputs=["split_train", "params:remove_out_train"],
+            outputs="train_data",
+            name='node_feat_enginerring_train',
+        ),
+        node(
+            func=feature_engineering,
+            inputs=["split_valid", "params:remove_out_valid"],
+            outputs="valid_data",
+            name='node_feat_enginerring_valid',
+        ),
+        node(
+            func=feature_engineering,
+            inputs=["test_raw_data", "params:remove_out_test"],
+            outputs="test_data",
+            name='node_feat_enginerring_test',
+        ),
+        node(
+            func=define_search_space,
+            inputs=None,
+            outputs='search_space',
+            name='define_search_space'
+        ),
+        node(
+            func=find_best_hyperparameters,
+            inputs=['search_space', 'train_data', 'valid_data', 'params:max_evals', 'params:alpha'],
+            outputs='best_hyperparams',
+            name='node_opt_bayes',
+        ),
+        node(
+            func=recreate_training,
+            inputs=['train_data', 'valid_data'],
+            outputs='training_data',
+            name='merge_train_valid',
+        ),
+        # node(
+        #     func=train_model_mlflow,
+        #     inputs=["training_data", "test_raw_data", "best_hyperparams"],
+        #     outputs="ml_model",
+        #     name='node_train_model'
+        # ),
+        node(
+            func=train_model,
+            inputs=["training_data", "best_hyperparams"],
+            outputs="ml_model",
+            name='node_train_model'
+        ),
+        node(
+            func=predict_model,
+            inputs=["ml_model", "training_data"],
+            outputs="pred_train",
+            name='node_pred_training',
+        ),
+        node(
+            func=predict_model,
+            inputs=["ml_model", "test_data"],
+            outputs="pred_test",
+            name="node_pred_test",
+        )
+    ])
