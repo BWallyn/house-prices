0223359c45a6bcc960f68aae58e11ba494f8a48e
BWallyn <ben96.wallyn@gmail.com>
2023-12-08 10:46:53 +0100

    fix names

--- src/house_prices/pipelines/data_science/pipeline.py
+++ src/house_prices/pipelines/data_science/pipeline.py
@@ -14,19 +14,19 @@ def create_pipeline(**kwargs) -> Pipeline:
             func=feature_engineering,
             inputs=["split_train", "params:remove_out_train"],
             outputs="train_data",
-            name='node_feat_enginerring_train',
+            name='node_feat_engineering_train',
         ),
         node(
             func=feature_engineering,
             inputs=["split_valid", "params:remove_out_valid"],
             outputs="valid_data",
-            name='node_feat_enginerring_valid',
+            name='node_feat_engineering_valid',
         ),
         node(
             func=feature_engineering,
             inputs=["test_raw_data", "params:remove_out_test"],
             outputs="test_data",
-            name='node_feat_enginerring_test',
+            name='node_feat_engineering_test',
         ),
         node(
             func=define_search_space,
@@ -61,7 +61,7 @@ def create_pipeline(**kwargs) -> Pipeline:
         node(
             func=predict_model,
             inputs=["ml_model", "training_data"],
-            outputs="pred_train",
+            outputs="pred_training",
             name='node_pred_training',
         ),
         node(
